a1297edc32e2809efc2ea93e815106760cb88e12
Javier Cejudo <javier@javiercejudo.com>
2015-09-04 00:35:56 +1000

    feat: modify presets schema

--- test/presets.js
+++ test/presets.js
@@ -4,11 +4,12 @@
 
 require('should');
 
-var rescale = require('rescale')(require('floating-adapter')).rescale;
-var mass = require('../src/linear-presets-mass');
+var Decimal = require('linear-arbitrary-precision')(require('floating-adapter'));
+var rescale = require('rescale')(Decimal).rescale;
+var mass = require('linear-preset-factory')(require('../src/linear-presets-mass'));
 
 function convert(x, preset) {
-  return rescale(x, preset[0], preset[1]);
+  return Number(rescale(x, preset[0], preset[1]));
 };
 
 function invert(preset) {
@@ -17,24 +18,26 @@ function invert(preset) {
 
 describe('mass presets', function() {
   it('should convert correctly', function() {
-    (10).should.be.exactly(convert(0.01, invert(mass.kilogramToMetricTon)), 'kilogramToMetricTon')
-      .and.exactly(convert(10000, invert(mass.kilogramToGram)), 'kilogramToGram')
-      .and.exactly(convert(1e+7, invert(mass.kilogramToMilligram)), 'kilogramToMilligram')
-      .and.exactly(convert(1e+10, invert(mass.kilogramToMicrogram)), 'kilogramToMicrogram')
-      .and.exactly(convert(0.00984251968503937, invert(mass.kilogramToLongTon)), 'kilogramToLongTon')
-      .and.exactly(convert(0.011023113109243879, invert(mass.kilogramToShortTon)), 'kilogramToShortTon')
-      .and.exactly(convert(1.5747304441776968, invert(mass.kilogramToStone)), 'kilogramToStone')
-      .and.exactly(convert(22.046226218487757, invert(mass.kilogramToPound)), 'kilogramToPound')
-      .and.exactly(convert(352.7396195580167, invert(mass.kilogramToOunce)), 'kilogramToOunce');
+    (10).should.be.exactly(convert(10, invert(mass.kilogram_kilogram)), 'kilogram_kilogram')
+      .and.exactly(convert(0.01, invert(mass.kilogram_metricTon)), 'kilogram_metricTon')
+      .and.exactly(convert(10000, invert(mass.kilogram_gram)), 'kilogram_gram')
+      .and.exactly(convert(1e+7, invert(mass.kilogram_milligram)), 'kilogram_milligram')
+      .and.exactly(convert(1e+10, invert(mass.kilogram_microgram)), 'kilogram_microgram')
+      .and.exactly(convert(0.00984251968503937, invert(mass.kilogram_longTon)), 'kilogram_longTon')
+      .and.exactly(convert(0.011023113109243879, invert(mass.kilogram_shortTon)), 'kilogram_shortTon')
+      .and.exactly(convert(1.5747304441776968, invert(mass.kilogram_stone)), 'kilogram_stone')
+      .and.exactly(convert(22.046226218487757, invert(mass.kilogram_pound)), 'kilogram_pound')
+      .and.exactly(convert(352.7396195580167, invert(mass.kilogram_ounce)), 'kilogram_ounce');
 
-    (0).should.be.exactly(convert(0, mass.kilogramToMetricTon), 'kilogramToMetricTon')
-      .and.exactly(convert(0, mass.kilogramToGram), 'kilogramToGram')
-      .and.exactly(convert(0, mass.kilogramToMilligram), 'kilogramToMilligram')
-      .and.exactly(convert(0, mass.kilogramToMicrogram), 'kilogramToMicrogram')
-      .and.exactly(convert(0, mass.kilogramToLongTon), 'kilogramToLongTon')
-      .and.exactly(convert(0, mass.kilogramToShortTon), 'kilogramToShortTon')
-      .and.exactly(convert(0, mass.kilogramToStone), 'kilogramToStone')
-      .and.exactly(convert(0, mass.kilogramToPound), 'kilogramToPound')
-      .and.exactly(convert(0, mass.kilogramToOunce), 'kilogramToOunce');
+    (0).should.be.exactly(convert(0, mass.kilogram_kilogram), 'kilogram_kilogram')
+      .and.exactly(convert(0, mass.kilogram_metricTon), 'kilogram_metricTon')
+      .and.exactly(convert(0, mass.kilogram_gram), 'kilogram_gram')
+      .and.exactly(convert(0, mass.kilogram_milligram), 'kilogram_milligram')
+      .and.exactly(convert(0, mass.kilogram_microgram), 'kilogram_microgram')
+      .and.exactly(convert(0, mass.kilogram_longTon), 'kilogram_longTon')
+      .and.exactly(convert(0, mass.kilogram_shortTon), 'kilogram_shortTon')
+      .and.exactly(convert(0, mass.kilogram_stone), 'kilogram_stone')
+      .and.exactly(convert(0, mass.kilogram_pound), 'kilogram_pound')
+      .and.exactly(convert(0, mass.kilogram_ounce), 'kilogram_ounce');
   });
 });
